eda0ae7599c88d1484db1426fb4aeafa09488b6c
ehcarey <144972660+ehcarey@users.noreply.github.com>
2025-03-21 19:53:32 -0500

    "login añadido"

---
+++ lib/presentation/screens/login.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Usuario',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción de inicio de sesión
+              },
+              child: Text('Iniciar sesión'),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                // Acción para recuperar contraseña
+              },
+              child: Text('Olvidé mi contraseña'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
